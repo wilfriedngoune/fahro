eb996765cd2c336be0f6fe938c9087c0a3fedf59
tijio <tatianatijio@gmail.com>
2025-12-16 17:41:58 +0100

    add trip

--- app/src/main/java/com/example/fahr/model/OnboardingPage.kt
+++ app/src/main/java/com/example/fahr/model/OnboardingPage.kt
@@ -1,6 +1,6 @@
 package com.example.fahr.model
 
-// Model used to describe one onboarding screen
+// Models used to describe one onboarding screen
 data class OnboardingPage(
     val title: String,
     val description: String,
